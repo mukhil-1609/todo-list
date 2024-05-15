3c5956a50dcdc09d463f0cef035105de211f5db8
mukhil-1609 <mukhilvarnan.dgl@gmail.com>
2024-05-15 05:58:00 +0530

    Initial commit

---
+++ my-todo-app/server/dbconfigure.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('database_name', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql', // Change this to match your SQL database
+});
+
+module.exports = sequelize;
---
+++ my-todo-app/server/models/Todo.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../dbconfigure');
+
+const Todo = sequelize.define('Todo', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Todo;
